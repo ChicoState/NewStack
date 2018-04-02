6ab880bd7adf28c9ae78d5bbf00e40fca0606a23
Oleksiy Al-saadi <alexmosenzov3@gmail.com>
2018-04-02 01:21:34 -0700

    StackWrapper class uses deque STL structure.

--- StackWrapper.h
+++ StackWrapper.h
@@ -1,7 +1,66 @@
-#ifndef STACKWRAP_H
-#define STACKWRAP_H
+// deque Stack class and StackWrapper class - Olekiy Al-saado
+#ifndef S_H
+#define S_H
 
-#include"Stack.h"
+#include<deque>
+
+using namespace std;
+
+
+/*
+* Delegates Queue from STL to Stack functionality.
+*/
+template<typename T>
+class Stack {
+	private:
+		deque<T> dequeStack;			
+	public:
+		void push(T);
+		T pop();
+		T peek();
+		int size();
+		void removeAll();	
+};
+
+template<typename T>
+void Stack<T>::push(T val){
+	dequeStack.push_front(val);
+}
+
+template<typename T>
+T Stack<T>::pop(){
+	if (dequeStack.size() == 0){
+		T null;
+		return null;
+	}
+	T ret = dequeStack.front();
+	dequeStack.pop_front();
+	return ret;
+	
+	
+}
+
+template<typename T>
+void Stack<T>::removeAll(){
+	while(!dequeStack.empty()){
+		dequeStack.pop_front();
+	}
+}
+
+template<typename T>
+int Stack<T>::size(){
+	return dequeStack.size();
+}
+
+template<typename T>
+T Stack<T>::peek(){
+	if (dequeStack.empty()){
+		T null;
+		return null;
+	}
+	return dequeStack.back();
+
+}
 
 
 /*
@@ -30,7 +89,7 @@ class StackWrapper {
 
 template<typename T>
 StackWrapper<T>::StackWrapper(){
-
+	
 }
 
 template<typename T>
